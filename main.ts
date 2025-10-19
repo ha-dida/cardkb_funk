fdeceecc4a195d2aeb57a4ebf9596459e7441a2c
hdr <dur.sifi@online.de>
2025-10-19 23:11:38 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+let input2 = ""
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.forever(function () {
+    input2 = String.fromCharCode(CardKB.readCharcode())
+    if (input2 != "") {
+        basic.showString(input2)
+        radio.sendString(input2)
+    }
+})
